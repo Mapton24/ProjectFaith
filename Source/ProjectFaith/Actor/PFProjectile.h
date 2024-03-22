9b45d793ebdcc333ad656fa5158adc733ba710d2
Mapton24 <robertosk555@gmail.com>
2024-03-22 21:13:40 +0100

    Added SpecHandle property for PFProjectile

--- Source/ProjectFaith/Actor/PFProjectile.h
+++ Source/ProjectFaith/Actor/PFProjectile.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
 #include "GameFramework/Actor.h"
 #include "PFProjectile.generated.h"
 
@@ -20,6 +21,9 @@ public:
 
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	FGameplayEffectSpecHandle DamageEffectSpecHandle;
+	
 
 protected:
 	// Called when the game starts or when spawned
